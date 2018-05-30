7b21b0ae628b4042d72129fec9d365c7fb41f5c8
blitnee <brittneelc@gmail.com>
2018-05-30 11:12:56 -0400

    feat: Add Reducers

--- index.js
+++ index.js
@@ -37,6 +37,35 @@ function createStore (reducer) {
 	 	dispatch
 	 }
 }
+/**
+ * Reducers: todos(), goals()
+ * A function that takes in the current state and an
+ * action that occurred, and returns the new state.
+ */
+function todos (state = [], action) {
+	switch(action.type) {
+		case ADD_TODO :
+			return state.concat([action.todo])
+		case REMOVE_TODO :
+			return state.filter((todo) => todo.id !== action.id)
+		case TOGGLE_TODO :
+			return state.map((todo) => todo.id !== action.id ? todo :
+			Object.assign({}, todo, { complete: !todo.complete }))
+		default :
+			return state
+	}
+}
+
+function goals (state = [], action) {
+	switch(action.type) {
+		case ADD_GOAL :
+			return state.concat([action.goal])
+		case REMOVE_GOAL :
+			return state.filter((goal) => goal.id !== action.id)
+		default :
+			return state
+	}
+}
 const store = createStore(app)
 
 store.subscribe(() => {
