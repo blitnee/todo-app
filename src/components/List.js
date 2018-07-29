3dfe233917986dffa015e5271b4af0cdbd110104
blitnee <brittneelc@gmail.com>
2018-07-29 15:52:56 -0400

    feat: Add List style structure

--- src/components/List.js
+++ src/components/List.js
@@ -4,13 +4,16 @@ export default function List (props) {
 	return (
 		<ul>
 			{props.items.map((item) => (
-				<li key={item.id}>
+				<li key={item.id} className='list-item'>
 					<span
+						className='list-item-text'
 						onClick={() => props.toggle && props.toggle(item.id)}
 						style={{textDecoration: item.complete ? 'line-through' : 'none'}}>
 						{item.name}
 					</span>
-					<button onClick={() => props.remove(item)}>X</button>
+					<button
+						className='delete-button'
+						onClick={() => props.remove(item)}>X</button>
 				</li>
 				))}
 		</ul>
